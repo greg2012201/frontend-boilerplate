c1954e026f34769dcd726622cbff1f70bb28ac11
Grzegorz Dubiel <greg2012201@gmail.com>
2021-01-23 15:30:21 +0100

    Remove babael from main webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -50,11 +50,7 @@ module.exports = {
 
       },
 
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
+
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
